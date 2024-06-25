788a4d939e7372f6d122efda861d6aba44cab277
christiantripodi <skl11@icloud.com>
2024-06-25 16:40:03 +0200

    mah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import CustomNavbar from './components/CustomNavbar'
+import fantasy from "./data/fantasy.json"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomNavbar />
+      <Container>
+        <Row>
+          {
+            fantasy.map(product => {
+              return (
+                <Col>
+                <Card>
+      <Card.Img variant="top" src="http://placedog.net/400" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
+      
     </div>
   );
 }
